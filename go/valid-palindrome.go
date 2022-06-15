e161cc3a49b0679edd564e332248c7fbc730bc80
David Olivares <daveolivares01@gmail.com>
2022-06-14 22:25:42 -0700

    Adds simpler go solution for valid palindrome

--- go/valid-palindrome.go
+++ go/valid-palindrome.go
@@ -1,32 +1,30 @@
 package leetcode
 
-import (
-	"log"
-	"regexp"
-	"strings"
-)
+import "strings"
 
 func isPalindrome(s string) bool {
-	s = processStringData(s)
-	j := len(s) - 1
-	for i := 0; i < len(s)/2; i++ {
-		if s[i] != s[j] {
+	i, j := 0, len(s)-1
+	for i < j {
+		if !validCharacter(s[i]) {
+			i++
+			continue
+		}
+		if !validCharacter(s[j]) {
+			j--
+			continue
+		}
+		if !strings.EqualFold(string(s[i]), string(s[j])) {
 			return false
 		}
+		i++
 		j--
 	}
 	return true
 }
 
-// helper func:
-// to lowercase
-// removes special characters
-func processStringData(s string) string {
-	s = strings.ToLower(s)
-	reg, err := regexp.Compile(`[^a-zA-Z0-9]`)
-	if err != nil {
-		log.Fatal(err)
+func validCharacter(a byte) bool {
+	if (a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z') || (a >= '0' && a <= '9') {
+		return true
 	}
-	s = reg.ReplaceAllString(s, "")
-	return s
+	return false
 }
