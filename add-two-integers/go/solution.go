cb987289dcd965f0bd20251ba9347119e1fb34f7
David Olivares <daveolivares01@gmail.com>
2024-01-15 19:41:43 -0800

    feat: adds Go & Java solutions for Add Two Integers

---
+++ add-two-integers/go/solution.go
@@ -0,0 +1,15 @@
+// sum calculates the sum of two integers.
+//
+// Time Complexity: O(1) as the addition operation is constant time.
+//
+// Space Complexity: O(1) as no additional data structures are used.
+//
+// Parameters:
+//   - num1: The first integer.
+//   - num2: The second integer.
+//
+// Returns:
+//   - The sum of num1 and num2.
+func sum(num1 int, num2 int) int {
+	return num1 + num2
+}
---
+++ add-two-integers/java/Solution.java
@@ -0,0 +1,20 @@
+/**
+ * Solution class provides a method to calculate the sum of two numbers.
+ */
+class Solution {
+    /**
+     * Calculates the sum of two numbers.
+     * 
+     * Time Complexity: O(1), as the addition operation is constant time.
+     * 
+     * Space Complexity: O(1), as there is no additional space used that grows with
+     * the input.
+     * 
+     * @param num1 The first integer.
+     * @param num2 The second integer.
+     * @return The sum of num1 and num2.
+     */
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
