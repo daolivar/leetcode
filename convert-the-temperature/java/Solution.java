a985c4149b66b705d44763e09fee142b42a12eef
David Olivares <daveolivares01@gmail.com>
2024-01-18 20:41:32 -0800

    chore: updates comment formatting for Convert the Temperature & Smallest Even Multiple

--- convert-the-temperature/java/Solution.java
+++ convert-the-temperature/java/Solution.java
@@ -1,16 +1,20 @@
 /**
- * Solution class provides a method to convert temperature from Celsius to Kelvin and Fahrenheit.
+ * Solution class provides a method to convert temperature from Celsius to
+ * Kelvin and Fahrenheit.
  */
 class Solution {
     /**
      * Converts temperature from Celsius to Kelvin and Fahrenheit.
      * 
-     * Time Complexity: O(1), as the conversion involves basic arithmetic operations that are constant time.
+     * Time Complexity: O(1), as the conversion involves basic arithmetic operations
+     * that are constant time.
      * 
-     * Space Complexity: O(1), as there is no additional space used that grows with the input.
+     * Space Complexity: O(1), as there is no additional space used that grows with
+     * the input.
      * 
      * @param celsius The temperature in Celsius.
-     * @return An array containing the converted temperature values in Kelvin and Fahrenheit.
+     * @return An array containing the converted temperature values in Kelvin and
+     *         Fahrenheit.
      *         Index 0: Temperature in Kelvin.
      *         Index 1: Temperature in Fahrenheit.
      */
@@ -18,4 +22,3 @@ class Solution {
         return new double[] { celsius + 273.15, celsius * 1.80 + 32.00 };
     }
 }
-
--- smallest-even-multiple/java/Solution.java
+++ smallest-even-multiple/java/Solution.java
@@ -1,16 +1,20 @@
 /**
- * Solution class provides a method to find the smallest even multiple of a given integer.
+ * Solution class provides a method to find the smallest even multiple of a
+ * given integer.
  */
 class Solution {
     /**
      * Finds the smallest even multiple of the given integer.
      * 
-     * Time Complexity: O(1), as the operation involves a simple conditional check and multiplication, both constant time.
+     * Time Complexity: O(1), as the operation involves a simple conditional check
+     * and multiplication, both constant time.
      * 
-     * Space Complexity: O(1), as there is no additional space used that grows with the input.
+     * Space Complexity: O(1), as there is no additional space used that grows with
+     * the input.
      * 
      * @param n The input integer.
-     * @return The smallest even multiple of the input integer. If the input is already even, returns the input itself.
+     * @return The smallest even multiple of the input integer. If the input is
+     *         already even, returns the input itself.
      */
     public int smallestEvenMultiple(int n) {
         return n % 2 == 0 ? n : 2 * n;
