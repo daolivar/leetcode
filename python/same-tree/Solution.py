4ea619d74b555f7d42d597c758e9772616a5ad26
David Olivares <daveolivares01@gmail.com>
2024-06-25 23:40:26 -0700

    feat: adds Python solution for Same Tree

---
+++ python/same-tree/Solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        """
+        Check if two binary trees are the same.
+
+        Parameters:
+        p (Optional[TreeNode]): The root of the first binary tree.
+        q (Optional[TreeNode]): The root of the second binary tree.
+
+        Returns:
+        bool: True if both trees are identical, False otherwise.
+        """
+        if not p and not q:
+            return True
+        if not p or not q or p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
