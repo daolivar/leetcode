a6c46d7cb3ba96544d4222646339b01174d70dfc
David Olivares <daveolivares01@gmail.com>
2024-06-28 15:03:24 -0700

    feat: adds Python solution for Remove Element

---
+++ python/remove-element/Solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        """
+        Remove all instances of `val` in-place and return the new length.
+
+        Parameters:
+        nums (List[int]): The list of integers from which to remove `val`.
+        val (int): The value to be removed from the list.
+
+        Returns:
+        int: The new length of the list after removing `val`.
+        """
+        j = 0  # Pointer to place the next valid element
+        for i in range(len(nums)):
+            if nums[i] != val:  # Check if the current element is not equal to val
+                nums[j] = nums[i]  # Place the valid element at position j
+                j += 1  # Increment the pointer j
+        return j  # Return the length of the array after removing val
