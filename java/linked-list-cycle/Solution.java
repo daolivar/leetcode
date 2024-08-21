8ff0b58f3cfee86d6a15da0b4a67b4f3e0f91960
David Olivares <daveolivares01@gmail.com>
2024-08-20 22:28:41 -0700

    refactor: simplifies Linked List Cycle solution for Java

--- java/linked-list-cycle/Solution.java
+++ java/linked-list-cycle/Solution.java
@@ -10,19 +10,20 @@ public class Solution {
      * @return true if there is a cycle in the linked list, false otherwise
      */
     public boolean hasCycle(ListNode head) {
-        if (head == null || head.next == null) {
-            return false; // If the list is empty or has only one node, no cycle can exist
+        if (head == null) {
+            return false; // If the list is empty no cycle can exist
         }
 
-        ListNode slow = head; // Slow pointer moves one step at a time
-        ListNode fast = head.next; // Fast pointer moves two steps at a time
+        ListNode slow = head;
+        ListNode fast = head;
 
         while (fast != null && fast.next != null) {
+            slow = slow.next; // Move slow pointer one step
+            fast = fast.next.next; // Move fast pointer two steps
+
             if (slow == fast) {
                 return true; // If the slow and fast pointers meet, a cycle exists
             }
-            slow = slow.next; // Move slow pointer one step
-            fast = fast.next.next; // Move fast pointer two steps
         }
 
         return false; // If we reach the end of the list, no cycle exists
